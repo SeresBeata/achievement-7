f3fb80eeeafed2c0a61147e30f885c47b7ee8407
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-04-14 17:32:35 +0200

    Update fetch-api-data.service.ts
    Import from rxjs and @angular. Declare the API URL that will provide data. Inject the HttpClient module to the constructor params. Make the API call for the user registration endpoint. Handle errors.

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+//import from rxjs
+import { Observable, catchError, throwError } from 'rxjs';
+//import from @angular/common/http
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+//import map
+import { map } from 'rxjs/operators';
+
+//Declaring the api url that will provide data for the client app
+const apiUrl = 'https://movie-myflix-c346f5fde8cf.herokuapp.com/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+  // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {}
+  // Making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
+  }
 
-  constructor() { }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
 }

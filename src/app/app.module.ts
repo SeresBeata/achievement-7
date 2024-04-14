c1d56d4933815170cf3bad1b89e9ada11949f851
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-04-14 16:32:14 +0200

    Set up Angular HttpClient
    Update app.module.ts file by importing HttpClientModule from '@angular/platform-browser', and adding HttpClientModule in the @NgModule imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+//import HttpClientModule, a simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side.
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//add HttpClientModule in the @NgModule imports
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

af8a3f8414161482464eceed86ef6a25b6250004
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-04-20 18:50:42 +0200

    Install Angular Material
    Install Angular Material by using Angular CLI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 //add HttpClientModule in the @NgModule imports
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  providers: [provideClientHydration()],
+  providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

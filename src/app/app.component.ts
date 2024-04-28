a404d0216413b3ca6e1231f1bc71c4a12feccba1
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-04-28 21:41:26 +0200

    Update app.component.ts with openMoviesDialog()
    Update app.component.ts file with openMoviesDialog() to open the dialog for displaying titles and images of movies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 import { MatDialog } from '@angular/material/dialog';
 
@@ -27,6 +28,14 @@ export class AppComponent {
     this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px',
+    });
+  }
+
+  //This is the function that will open the dialog when the All Movies button is clicked
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '400px',
+      height: '500px',
     });
   }
 }

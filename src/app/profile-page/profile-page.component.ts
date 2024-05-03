ca5789e0624d0df6c15c78ed9d30e5a8fa4660f5
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-05-03 12:43:38 +0200

    Update profile-page.component.ts
    Update profile-page.component.ts with getUser() to get data about the user and editUser() to update data of the user.

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+
+type User = {
+  _id?: string;
+  username?: string;
+  password?: string;
+  email?: string;
+  birthday?: string;
+  favouriteMovies?: [];
+};
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrl: './profile-page.component.scss'
+  styleUrl: './profile-page.component.scss',
 })
 export class ProfilePageComponent {
+  user: User = {};
 
+  @Input() userData = { username: '', password: '', email: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const user = this.getUser();
+
+    if (!user._id) {
+      this.router.navigate(['welcome']);
+      return;
+    }
+
+    this.user = user;
+    this.userData = {
+      username: user.username || '',
+      password: '',
+      email: user.email || '',
+    };
+  }
+
+  getUser(): User {
+    return JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  editUser(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+
+      // testing:
+      console.log('updateUser called');
+      console.log('result:', result);
+      alert('Update was succesful ' + result.username + '!');
+
+      this.user = result;
+    });
+  }
 }
